69c6ec6752fe31bd19a33762919286363da02ccf
mohammadhosein dehghani <mohammadhosein00132@outlook.com>
2025-08-31 10:26:38 +0330

    fix some issues

--- MiniMessanger/Application Services/Services/UserService.cs
+++ MiniMessanger/Application Services/Services/UserService.cs
@@ -1,6 +1,56 @@
-﻿namespace MiniMessenger.Application.Services;
+﻿using MiniMessenger.Domain.Entities;
+using MiniMessenger.Domain.Interfaces.Service_Contracts;
+using MiniMessenger.Infrastructure;
 
-public class UserService
+namespace MiniMessenger.Application_Services.Services;
+
+public class UserService(FileRepository repo, Session session) : IUserService
 {
-    
+    private readonly FileRepository _repo = repo;
+    private readonly Session _session = session;
+
+    public void Register(string userName, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public User Login(string userName, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ChangePassword(string oldPassword, string newPassword)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ChangeStatus(string status)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<User> Search(string userName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool SendMessage(string toUsername, string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Message> ShowInbox()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Message> ShowSendBox()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Logout()
+    {
+        throw new NotImplementedException();
+    }
 }
